b2298b29bf1d9df46ddb2b6c2bf55ad665c47048
biautollanta <bi@autollanta.com>
2026-08-19 22:35:10 +0000

    feat: agregar reporte comercial tecnicentro antiguo

---
+++ src/pages/Areas/Reporteria/ComercialTecnicentroOld/ReporteriaComercialTecnicentroOld.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { TemplateReporteria } from "../TemplateReporteria";
+
+const REPORTES = [
+    {
+        id: 0,
+        url: "https://app.powerbi.com/view?r=eyJrIjoiYzMwYjZmMjQtNThiMS00NjdhLThjZDktMDAwMjIwZWI4MzM0IiwidCI6IjI0ODVhZjVjLWEzZTEtNGE4NS05MTBiLTc5NTIzOTQwYTk3MSJ9",
+        titulo: "Reporte Comecial Tenicentro Antiguo",
+        rol: "usuario",
+        linea: null,
+        empresa: "AUTOLLANTA",
+    },
+];
+
+export const ReporteriaComecialTecnicentroOld = ({
+    routeConfig,
+    availableCompanies = [],
+    availableLines = [],
+    availableCanales,
+}) => {
+    return (
+        <TemplateReporteria
+            reportes={REPORTES}
+            routeConfig={routeConfig}
+            availableCompanies={availableCompanies}
+            availableLines={availableLines}
+            availableCanales={availableCanales}
+        />
+    );
+};
